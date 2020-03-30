e859d635e002ee2cb3e06fc64a58605b61d1b109
riededom <62891426+riededom@users.noreply.github.com>
2020-03-30 16:24:07 +0200

    Add files via upload

---
+++ MainActivity.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using Android;
+using Android.App;
+using Android.Content.PM;
+using Android.OS;
+using Android.Support.V7.App;
+using Android.Views;
+using Android.Widget;
+using PDFExport;
+using Environment = Android.OS.Environment;
+using Path = System.IO.Path;
+
+namespace ApitronTestApp1
+{
+    [Activity(Label = "@string/app_name", Theme = "@style/AppTheme.NoActionBar", MainLauncher = true)]
+    public class MainActivity : AppCompatActivity
+    {
+        public MainActivity()
+        {
+            pdf = new PdfExporter();
+        }
+
+        private PdfExporter pdf;
+
+        protected override void OnCreate(Bundle savedInstanceState)
+        {
+            base.OnCreate(savedInstanceState);
+            SetContentView(Resource.Layout.activity_main);
+            Android.Support.V7.Widget.Toolbar toolbar =
+            FindViewById<Android.Support.V7.Widget.Toolbar>(Resource.Id.toolbar);
+            SetSupportActionBar(toolbar);
+            CheckAppPermissions();
+            //pdf = new PdfExporter();
+            Button fab = FindViewById<Button>(Resource.Id.button);
+            fab.Click += SaveDocument;
+        }
+
+        public override bool OnCreateOptionsMenu(IMenu menu)
+        {
+            MenuInflater.Inflate(Resource.Menu.menu_main, menu);
+            return true;
+        }
+
+        private void SaveDocument(object sender, EventArgs e)
+        {
+            pdf.SaveDocument(GetFilePath());
+        }
+
+        public override bool OnOptionsItemSelected(IMenuItem item)
+        {
+            int id = item.ItemId;
+            if (id == Resource.Id.action_settings)
+            {
+                return true;
+            }
+
+            return base.OnOptionsItemSelected(item);
+        }
+
+        private void CheckAppPermissions()
+        {
+            if (PackageManager.CheckPermission(Manifest.Permission.ReadExternalStorage, PackageName) != Permission.Granted ||
+                PackageManager.CheckPermission(Manifest.Permission.WriteExternalStorage, PackageName) != Permission.Granted)
+            {
+                var permissions = new string[] { Manifest.Permission.ReadExternalStorage, Manifest.Permission.WriteExternalStorage };
+                RequestPermissions(permissions, 1);
+            }
+        }
+
+        private string GetFilePath()
+        {
+            String path = Path.Combine(Environment.GetExternalStoragePublicDirectory(Environment.DirectoryDownloads).AbsolutePath,
+                "jjj.pdf");
+            return path;
+        }
+    }
+}
+
---
+++ Properties/AssemblyInfo.cs
@@ -0,0 +1,30 @@
+﻿using System.Reflection;
+using System.Runtime.InteropServices;
+using Android.App;
+
+// General Information about an assembly is controlled through the following 
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("ApitronTestApp1")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("Leica Geosystems")]
+[assembly: AssemblyProduct("Argos Mobile")]
+[assembly: Apitron.Licensing.ApitronPDFKitLicense("eJyz8clMTs0rTrWzCSjKTylNLrFzLMgsKcrP0wtwcdPzzixRMNQzUHAsKMjJTE4syczPg6q30Yept3EuLS7Jz00tsvNJBapRcE/NL64sLknNLbbRh0vZIJlg51iUnl+s4JuflJkDNAdZxsazuLg01SWxJNXO0FTfwELfyMDQ0kYfIWqjD3Nv4UeDgzouaetjBW7PaZXtqL7xvv2KV31f7f+qRdkmD7fIifzKvXFxevR0tkWzi9ovez2XSGS7O8Gfc17VZP5YlZU7vP6X6rz71DB5RbwIh4/zr9mPtW85u2UUrJl6ZfvKPT+9ktJuc3+ctrmlzDN69dICr3e34xMaYp6yS3G/qZm+7/GVlvOXAWWehIE=")]
+[assembly: AssemblyCopyright("Copyright ©  2018")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+[assembly: ComVisible(false)]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version 
+//      Build Number
+//      Revision
+//
+// You can specify all the values or you can default the Build and Revision Numbers 
+// by using the '*' as shown below:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
